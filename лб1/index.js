62ef8baff5753cbe4b0e7a6756aaad9a7a4c8a5d
nickless37 <daniil21022006@gmail.com>
2025-09-10 09:17:44 +0300

    commit message

--- лб1/index.js
+++ лб1/index.js
@@ -1,1 +1,8 @@
-//console.log (`hello Danua`) 
+//console.log (`hello Daniil`) 
+const button = document.getElementById("B1")
+const text = document.getElementById("v3")
+button.addEventListener("click", () => {
+    alert('Hello Daniil')
+    console.log (`Hello Daniil`)
+    text.textContent = `Hello Daniil`
+});
