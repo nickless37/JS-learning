82975cf713e2bf5368d137e9e4d76bfe1dab354c
Danua <137786976+nickless37@users.noreply.github.com>
2025-10-15 18:49:18 +0300

    added page

---
+++ самостійна робота 1/code.js
@@ -0,0 +1,7 @@
+document.getElementById("vis").addEventListener("click", ()=> {
+    window.open('https://nickless37.github.io/web-design-2025-Timohin/', '_blank')
+})
+
+document.getElementById("code").addEventListener("click", ()=> {
+    window.open('https://github.com/nickless37/web-design-2025-Timohin', '_blank')
+})
