7a57da36de66d526d2389d677089429e3a47e341
Danua <137786976+nickless37@users.noreply.github.com>
2025-11-01 22:01:32 +0200

    theme swithcer recovery point

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
 import myPhoto from './images/template1.png';
 import Frontend from './images/frontend.png';
 import Backend from './images/backend.png';
 import Java from './images/java.png';
+import Python from './images/python.png'
 
 function App() {
   return (
     <div className="App min-vh-100 bg-dark text-light">
       <header className="App-header">
         <h1>Резюме</h1>
+        <Form className='d-flex justify-content-end pe-5'>
+          <Form.Check
+            type="switch"
+            id="custom-switch"
+            label="theme"
+          />
+        </Form>
         <div className='about_me d-flex justify-content-center ms-5 me-5 mt-5'>
           <img 
             src={myPhoto} 
@@ -35,40 +45,53 @@ function App() {
         <Carousel>
           <Carousel.Item>
             <img 
-              className="d-block w-100"
+              className="img-size-custom"
               src={Frontend} 
               alt="Frontend"
-              style={{ height: '400px', objectFit: 'cover' }}
+
             />
-            <Carousel.Caption>
-              <h3>Frontend Development</h3>
-              <p>HTML, CSS, JavaScript projects</p>
+            <Carousel.Caption className='bg-dark text-light bg-opacity-75 p-3 rounded labelBackground'>
+              <h3>Frontend</h3>
+              <p>HTML, CSS</p>
             </Carousel.Caption>
           </Carousel.Item>
           
           <Carousel.Item>
             <img 
-              className="d-block w-100"
+              className="img-size-custom"
               src={Backend} 
               alt="Backend"
-              style={{ height: '400px', objectFit: 'cover' }}
+
             />
-            <Carousel.Caption>
-              <h3>Backend Development</h3>
-              <p>Server-side applications</p>
+            <Carousel.Caption className='bg-light text-dark bg-opacity-75 p-3 rounded labelBackground'>
+              <h3>Backend</h3>
+              <p>Java script</p>
             </Carousel.Caption>
           </Carousel.Item>
           
           <Carousel.Item>
             <img 
-              className="d-block w-100"
+              className="img-size-custom"
               src={Java} 
               alt="Java"
-              style={{ height: '400px', objectFit: 'cover' }}
+
             />
-            <Carousel.Caption>
-              <h3>Java Projects</h3>
-              <p>Java applications and systems</p>
+            <Carousel.Caption className='bg-light text-dark bg-opacity-75 p-3 rounded labelBackground' >
+              <h3>Java</h3>
+              <p>Java applications</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img 
+              className="img-size-custom"
+              src={Python} 
+              alt="Python"
+
+            />
+            <Carousel.Caption className='bg-light text-dark bg-opacity-75 p-3 rounded labelBackground'>
+              <h3>Python</h3>
+              <p>Python applications</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
